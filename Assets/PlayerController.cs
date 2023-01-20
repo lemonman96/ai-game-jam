90b036a7256f4544e4d878215f669767ff03b46e
lemonman96 <lennon.scariano@uga.edu>
2023-01-19 23:33:33 -0500

    added sword animation + hitbox

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,36 +4,54 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public Vector3 InputDir;
-    public bool Alive;
-    public int HP, MHP, MP, MMP, STR, DEF, ATK, AGL, EXP, SPD;
+    public GameObject sword;
+    public bool isAlive, isSwinging;
+    public int max_hp, max_ap, current_hp, current_ap, speed, running_speed;
     public string Name;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        HP = MHP;
-        MP = MMP;
+        current_hp = max_hp;
+        current_ap = max_ap;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        InputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        
+        Vector3 InputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        
+        
 
         if(InputDir != Vector3.zero)//Moving!  
         {
-
             //do the move here!
-            transform.position += InputDir * (SPD * .01f);
+            transform.position += InputDir * (speed * 0.1f);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            sword.GetComponent<Animator>().SetTrigger("isSwinging");
+            Collider2D[] collisions = Physics2D.OverlapBoxAll(new Vector2(sword.transform.position.x, sword.transform.position.y), 
+            new Vector2(sword.transform.localScale.x, sword.transform.localScale.y), sword.transform.localEulerAngles.z);
+            foreach(Collider2D collider2D in collisions) {
+                print(collider2D);
+            }
+            //print(ac.parameters);
+        }
+
+        if(sword.GetComponent<Animator>().GetParameter(0).Equals(true)) {
+            
+            
 
         }
-        else//not moving!
-        {
-            
-        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
+            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+            Gizmos.DrawWireCube(sword.transform.position, sword.transform.localScale);
+    
     }
 }
